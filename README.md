60c60220d94529896a1774ada131d4cf873a01c5
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-01-31 00:32:27 +0500

    feat: Init. (#1)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # prettier-plugin-prisma
+
 Prettier plugin for Prisma
+
+### Installation
+
+```bash
+# Since `@prisma/sdk` version should match your `@prisma/client` version
+# its a peer dependency.
+npm i -D @prisma/sdk prettier-plugin-prisma
+
+# or with yarn
+yarn add -D @prisma/sdk prettier-plugin-prisma
+```
---
+++ src/plugin.ts
@@ -0,0 +1,44 @@
+import { spawnSync } from "child_process";
+import { Plugin } from "prettier";
+
+export default {
+  languages: [
+    {
+      name: "Prisma",
+      extensions: [".prisma"],
+      parsers: ["prisma-parse"],
+    },
+  ],
+  parsers: {
+    "prisma-parse": {
+      astFormat: "prisma-ast",
+      parse: (text) => text,
+      locStart: () => 0,
+      locEnd: (node) => node.length,
+    },
+  },
+
+  printers: {
+    "prisma-ast": {
+      print: (path) => {
+        const { stdout, stderr } = spawnSync(
+          "node",
+          [
+            "-e",
+            "require('@prisma/sdk').formatSchema({ schema: require('fs').readFileSync(process.stdin.fd, 'utf8') }).then(console.log)",
+          ],
+          {
+            encoding: "utf8",
+            input: path.getValue(),
+          }
+        );
+
+        if (stderr) {
+          throw new Error(stderr);
+        }
+
+        return stdout;
+      },
+    },
+  },
+} as Plugin<string>;
