98a6eba622004e053bbb7528aa35f1d5eed8edda
Umidbek Karimov <uma.karimov@gmail.com>
2021-01-31 00:32:27 +0500

    feat: Init. (#1)

---
+++ src/plugin.spec.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from "fs";
+import { join as joinPath } from "path";
+import { check, format } from "prettier";
+import plugin from "./plugin";
+
+const formatted = readFileSync(
+  joinPath(__dirname, "__fixtures__", "formatted.prisma"),
+  "utf8"
+);
+
+const unformatted = readFileSync(
+  joinPath(__dirname, "__fixtures__", "unformatted.prisma"),
+  "utf8"
+);
+
+test("basic", () => {
+  expect(
+    format(formatted, {
+      plugins: [plugin],
+      filepath: "./prisma/schema.prisma",
+    })
+  ).toBe(formatted);
+
+  expect(
+    format(unformatted, {
+      plugins: [plugin],
+      filepath: "./prisma/schema.prisma",
+    })
+  ).toBe(formatted);
+
+  expect(
+    check(formatted, {
+      plugins: [plugin],
+      filepath: "./prisma/schema.prisma",
+    })
+  ).toBe(true);
+
+  expect(
+    check(unformatted, {
+      plugins: [plugin],
+      filepath: "./prisma/schema.prisma",
+    })
+  ).toBe(false);
+});
