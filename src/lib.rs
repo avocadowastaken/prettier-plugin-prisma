4b4a0278d4479a38547a71e67059b4061f596e62
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-09-07 23:11:03 +0300

    chore: Simplify rust structure.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
-use datamodel::ast::reformat::Reformatter;
-
 #[wasm_bindgen]
 pub fn format(input: &str) -> String {
-    Reformatter::new(input).reformat_to_string()
+    datamodel::ast::reformat::Reformatter::new(input).reformat_to_string()
 }
