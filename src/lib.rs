21c35b9aa34833cacd2db47022ea1646b52f6250
Umidbek Karimov <4734297+umidbekk@users.noreply.github.com>
2021-12-01 22:30:21 +0200

    feat: Migrate to `@prisma/prisma-fmt-wasm`. (#44)

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-extern crate wasm_bindgen;
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn format(input: &str) -> String {
-    datamodel::ast::reformat::Reformatter::new(input).reformat_to_string()
-}
